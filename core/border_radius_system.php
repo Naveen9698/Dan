9b59319868d8e08a20a2e6434442b20e7a1baf15
Naveen Shanmugam <naveen.shanmugam@minorilabs.com>
2026-05-06 13:27:07 +0530

    new laptp

---
+++ core/border_radius_system.php
@@ -0,0 +1,21 @@
+<h2 class="d-h2 demo" id="space-system"> System</h2>
+
+<section class="d-section">
+  <h3 class="d-h3 demo">Variables</h3>
+
+  <div class="d-cols">
+    <pre><code><?php include 'class/1.php'; ?></code></pre>
+  </div>
+
+  <p class="d-note"></p>
+
+  <h3 class="d-h3 demo">Utility Classes</h3>
+
+  <div class="d-cols">
+    <pre><code><?php include 'class/2.php'; ?></code></pre>
+    <pre><code><?php include 'class/3.php'; ?></code></pre>
+  </div>
+
+  <h3 class="d-h3 demo">Live Demo</h3>
+
+</section>
